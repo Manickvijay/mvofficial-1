9b5d25cc7e28f43ff0171c69fb6f9d1817129f74
Mv <97879877+Manickvijay@users.noreply.github.com>
2024-04-12 07:48:13 +0530

    Update contact.js

--- forms/contact.js
+++ forms/contact.js
@@ -1,39 +1,24 @@
-const fname = document.getElementById('fname');
-
-const lname = document.getElementById('lname');
-
-const submit = document.getElementById('submit');
-
-
+const cname = document.getElementById('name');
+const cemail = document.getElementById('email');
+const csubject = document.getElementById('subject');
+const cmessage = document.getElementById('message');
 
 submit.addEventListener('submit',(e)=>{
-
     e.preventDefault();
-
     let ebody = `
-
-    <h1>First Name: </h1>${fname.value}
-
+    <h1>Email: </h1>${cemail.value}
     <br>
-
-    <h1>Last Name: </h1>${lname.value}
-
+    <h1>Name: </h1>${cname.value}
+    <br>
+    <h1>Message: </h1>${cmessage.value}
     `;
-
     Email.send({
-
         Host : "smtp.elasticemail.com",
-
         Username : "manickvijay596@gmail.com",
-
         Password : "C199EB33FAA152AF3BF82FAE35D07739B3A0",
-
         To : 'manickvijay596@gmail.com', 
-
         From : "manickvijay596@gmail.com",
-
-        Subject : "Help for mvprofilepage",
-
+        Subject : ${csubject.value},
         Body : ebody
 
     }).then(
